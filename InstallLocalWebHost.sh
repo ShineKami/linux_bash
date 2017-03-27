d6bb5cf1167a18136d4d911f3f305a36cba1dcc8
Stanislav Vyshynskyi <shinekami@madys.net>
2017-03-27 20:38:34 +0300

    Add auto-configure php-fpm and enable auto-load

--- InstallLocalWebHost.sh
+++ InstallLocalWebHost.sh
@@ -3,11 +3,27 @@
 
 if [ "`lsb_release -is`" == "LinuxMint" ] || [ "`lsb_release -is`" == "Debian" ] || [ "`lsb_release -is`" == "Ubuntu" ]
 then
+	sudo apt-get update;
+
+	#Install ngix
+	sudo apt-get -y install nginx;
+	sudo systemctl enable nginx;
+
 	#Install mysql
-	sudo apt-get -y install mysql-server mysql-client libmysqld-dev;
+	sudo apt-get -y install mysql-server;
+	sudo mysql_secure_installation;
+	sudo systemctl mysql-server mysql-server;
 
 	#Install PHP7.0
-	sudo apt-get -y install php7.0-cli php7.0-common php7.0 php7.0-mysql php7.0-fpm php7.0-curl php7.0-gd php7.0-bz2 php7.0-mcrypt php7.0-curl;
+	sudo apt-get -y install php7.0-fpm php7.0-cli php7.0-common php7.0-mysql;
+	sudo sed 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.0/fpm/php.ini > /etc/php/7.0/fpm/php.out;
+	sudo rm /etc/php/7.0/fpm/php.ini;
+	sudo mv /etc/php/7.0/fpm/php.out /etc/php/7.0/fpm/php.ini;
+	sudo sed 's/listen = /var/run/php7.0-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.0/fpm/pool.d/www.conf > /etc/php/7.0/fpm/pool.d/www.out;
+	sudo rm /etc/php/7.0/fpm/pool.d/www.conf;
+	sudo mv /etc/php/7.0/fpm/pool.d/www.out /etc/php/7.0/fpm/pool.d/www.conf;
+	sudo system restart php7.0-fpm;
+	sudo systemctl enable php-7.0-fpm;
 
 	#Addition install
 	sudo apt-get -y install phpmyadmin;
